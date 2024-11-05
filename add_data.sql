6c2faeb31dddff9cf2915c684e8789a4719f5577
maver-ya <maver2005@ya.ru>
2024-11-05 23:53:13 +0300

    db scripts

--- add_data.sql
+++ add_data.sql
@@ -3,3 +3,4 @@ INSERT INTO tasks (title, status) VALUES ('Задача 2', 'completed');
 INSERT INTO tasks (title, status) VALUES ('Задача 3', 'pending');
 INSERT INTO tasks (title, status) VALUES ('Задача 4', 'in progress');
 INSERT INTO tasks (title, status) VALUES ('Задача 5', 'completed');
+INSERT INTO tasks (title, status) VALUES ('Задача 6', 'pending');
